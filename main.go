aa9d1dbf43ca8e1247817446ddbaea0a8a30603a
nuttakit kundum <effy.nuttakit@bitkub.com>
2022-12-23 07:01:33 +0700

    add main.go

--- main.go
+++ main.go
@@ -1,26 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"context"
-	"log"
-	"math/big"
-	
-	"github.com/ethereum/go-ethereum/common"
+	"fmt"
+	"kub-report/balancereport"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"time"
 )
 
 func main() {
-	ctx := context.Background()
+	// Connect to the Ethereum mainnet using the Infura RPC URL
 	client, err := ethclient.Dial("https://mainnet.infura.io/v3/8e832100bb9d451887b920a0935dc120")
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error connecting to Ethereum mainnet: %v\n", err)
+		return
 	}
-	account := common.HexToAddress("0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8")
-	blockNumber := big.NewInt(16241410)
-	balance, err := client.BalanceAt(ctx, account, blockNumber)
+
+	// Create a new balance report instance
+	br, err := balancereport.New(client)
 	if err != nil {
-	log.Fatal(err)
+		fmt.Printf("Error creating balance report: %v\n", err)
+		return
 	}
-	fmt.Println(balance)
+
+	// Set the start and end timestamps for the report
+	startTimestamp := time.Date(2020, 12, 1, 0, 0, 0, 0, time.UTC).Unix()
+	endTimestamp := time.Date(2020, 12, 10, 0, 0, 0, 0, time.UTC).Unix()
+
+	// Set the addresses to include in the report
+	addresses := []string{
+		"0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a",
+		"0x742d35cc6634c0532925a3b844bc454e4438f44e",
+		"0xc94770007dda54cF92009BFF0dE90c06F603a09f",
+	}
+
+	// Generate the balance report
+	if err := br.GetReport(context.Background(), startTimestamp, endTimestamp, addresses); err != nil {
+		fmt.Printf("Error generating balance report: %v\n", err)
+		return
+	}
+
+	fmt.Println("Balance report generated successfully!")
 }
